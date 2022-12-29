94c649b85b098cc0c64b0dce345237be05414b54
Vincent Maag <vincentmaag@gmx.ch>
2022-12-29 13:32:58 +0100

    added readClockStreched() function to class
    this function can be used if a i2c device uses clock stretching. We basically just wait between sending a cmd and reading the response

--- maag_i2c_device.cpp
+++ maag_i2c_device.cpp
@@ -32,10 +32,10 @@ void MaagI2CDevice::setDeviceAddress(uint8_t dev_addr_)
     m_dev_addr = dev_addr_;
 }
 
-esp_err_t MaagI2CDevice::read(uint8_t *data_reg_, size_t data_reg_size_, uint8_t *in_data_, size_t in_size_)
+esp_err_t MaagI2CDevice::read(uint8_t *data_reg_, size_t data_reg_size_, uint8_t *in_data_, size_t in_data_size_)
 {
     // check input and output data is valid
-    if(data_reg_ == NULL || in_data_ == NULL || data_reg_size_ < 1 || in_size_ < 1){
+    if(data_reg_ == NULL || in_data_ == NULL || data_reg_size_ < 1 || in_data_size_ < 1){
         ESP_LOGE(TAG, "Invalid buffer pointers or invalid size");
         return ESP_ERR_INVALID_ARG;
     }
@@ -47,10 +47,51 @@ esp_err_t MaagI2CDevice::read(uint8_t *data_reg_, size_t data_reg_size_, uint8_t
     // write slave address, reading bit and get data that we requested in last command
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (m_dev_addr << 1) | I2C_MASTER_READ, true);
-    i2c_master_read(cmd, in_data_, in_size_, I2C_MASTER_LAST_NACK);
+    i2c_master_read(cmd, in_data_, in_data_size_, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd);
     // start sequence
     esp_err_t res = i2c_master_cmd_begin(m_port, cmd, I2CDEV_TIMEOUT / portTICK_PERIOD_MS);
+    if (res != ESP_OK)
+        ESP_LOGE(TAG, "Could not read from device [0x%02x at %d]: %d", m_dev_addr, m_port, res);
+    i2c_cmd_link_delete(cmd);
+
+    return res;
+}
+
+esp_err_t MaagI2CDevice::readClockStretched(uint8_t *data_reg_, size_t data_reg_size_, uint8_t *in_data_, size_t in_data_size_, int delay_ms_)
+{
+    // check input and output data is valid
+    if(data_reg_ == NULL || in_data_ == NULL || data_reg_size_ < 1 || in_data_size_ < 1){
+        ESP_LOGE(TAG, "Invalid buffer pointers or invalid size");
+        return ESP_ERR_INVALID_ARG;
+    }
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    // write slave address, writing bit and which register we want to read from slave afterwards
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (m_dev_addr << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write(cmd, data_reg_, data_reg_size_, true);
+    i2c_master_stop(cmd);
+    esp_err_t res = i2c_master_cmd_begin(m_port, cmd, I2CDEV_TIMEOUT / portTICK_PERIOD_MS);
+    if (res != ESP_OK)
+        ESP_LOGE(TAG, "Could not send cmd to device [0x%02x at %d]: %d", m_dev_addr, m_port, res);
+    i2c_cmd_link_delete(cmd);
+
+    //ESP_LOGI(TAG, "Sent cmd to device [0x%02x at %d]: %d", m_dev_addr, m_port, res);
+
+    //ESP_LOGI(TAG, "pausing shortly...");
+    
+    // delay for 100ms between write and read calls, as the sensor can take
+    // some time to respond.
+    vTaskDelay(delay_ms_ / portTICK_RATE_MS);
+
+    // get response data after a pause
+    cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (m_dev_addr << 1) | I2C_MASTER_READ, true);
+    i2c_master_read(cmd, in_data_, in_data_size_, I2C_MASTER_LAST_NACK);
+    i2c_master_stop(cmd);
+    // start sequence
+    res = i2c_master_cmd_begin(m_port, cmd, I2CDEV_TIMEOUT / portTICK_PERIOD_MS);
     if (res != ESP_OK)
         ESP_LOGE(TAG, "Could not read from device [0x%02x at %d]: %d", m_dev_addr, m_port, res);
     i2c_cmd_link_delete(cmd);
