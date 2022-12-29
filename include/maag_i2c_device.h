eb76f2ca3b1d25ed94185f3024bbe0b40f07a548
Vincent Maag <vincentmaag@gmx.ch>
2022-12-29 14:03:13 +0100

    Revert "added readClockStreched() function to class"
    This reverts commit 94c649b85b098cc0c64b0dce345237be05414b54.

--- include/maag_i2c_device.h
+++ include/maag_i2c_device.h
@@ -8,7 +8,15 @@
 #include <time.h>
 #include "driver/i2c.h"
 
+
+// #define DS3231_12HOUR_FLAG  0x40
+// #define DS3231_12HOUR_MASK  0x1f
+// #define DS3231_PM_FLAG      0x20
+// #define DS3231_MONTH_MASK   0x1f
+
 #define I2CDEV_TIMEOUT 1000
+
+// class to create a I2C device
 
 class MaagI2CDevice
 {
@@ -24,12 +32,9 @@ public:
     void setPort(i2c_port_t port_);
     // set device address
     void setDeviceAddress(uint8_t dev_addr_);
-    // read a registry from device, sending standard cmds and directly reading response
-    esp_err_t read(uint8_t *data_reg_, size_t data_reg_size_, uint8_t *in_data_, size_t in_data_size_);
-    // read a register from a device that probably uses clock stretching between receiving command and sending back response.
-    // A manual delay is implemented between sending cmd and requesting response
-    esp_err_t readClockStretched(uint8_t *data_reg_, size_t data_reg_size_, uint8_t *in_data_, size_t in_data_size_, int delay_ms_);
-    // write to a device register
+    // read from device
+    esp_err_t read(uint8_t *out_data, size_t out_size, uint8_t *in_data, size_t in_size);
+    // write to device
     esp_err_t write(uint8_t *data_reg_, size_t data_reg_size_, uint8_t *out_data_, size_t out_data_size_);
 
 };
