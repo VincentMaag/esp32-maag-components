55b46aa1e259b267d005792d8934010586a4b482
Vincent Maag <vincentmaag@gmx.ch>
2023-01-01 12:21:04 +0100

    added "spi mode" to maag_spi_device.h

--- include/maag_spi_device.h
+++ include/maag_spi_device.h
@@ -22,7 +22,7 @@ public:
     //MaagSpiDevice(spi_host_device_t host_, int clock_speed_hz_, gpio_num_t cs_gpio_nr_);
     
     // Create a SPI device and connect it to a spi-host. Initialize chip-select
-    esp_err_t initDevice(spi_host_device_t host_, int clock_speed_hz_, gpio_num_t cs_gpio_nr_);
+    esp_err_t initDevice(spi_host_device_t host_, int clock_speed_hz_, uint8_t mode_, gpio_num_t cs_gpio_nr_);
     // write to spi device
     esp_err_t write_bytes(uint8_t *out_data_, uint8_t out_data_size_);
     // select chip
